816c08b0c886b2f7c469f183fe84d1fe188222b4
Brendan <breichhoff@wisc.edu>
2017-10-04 20:00:00 -0500

    Implemented exit, pwd, and cd. Finished unimplemented command calls.

--- p2/mysh.c
+++ p2/mysh.c
@@ -2,48 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
 
 const char error_message[30] = "An error has occured\n";
 int doWait = 0;
-
-void changeDir(char *userInput){
-
-}
-
-void printWD(){
-
-}
-
-void exitProgram(){
-
-}
-
-//add forking here.
-int callCommand(char *userInput){
+char **parsedArgs;
+void parseInput(char *userInput){
   char *savePtr;
   char *splitInput = malloc(sizeof(char) * 129);
-  char **args = malloc(sizeof(char *) * 65);
+  char **args = malloc(sizeof(char *) * 2);
   splitInput = strtok_r(userInput, " ", &savePtr);
   int index = 0;
   while(splitInput!=NULL){
     args[index++] = splitInput;
     splitInput = strtok_r(NULL, " ", &savePtr);
   }
-  args[index] = NULL;
-
-  int pid = fork(), status;
-  if((pid == 0) && (execvp(args[0], args) == -1)){
-    return 0;
+  free(splitInput);
+  parsedArgs = args;
+}
+void changeDir(char **args){
+  if(args[1] != NULL && strlen(args[1])>0){
+    if(args[1][0] == '/'){
+      chdir(args[1]);
+    }else{
+      char directory[129];
+      getcwd(directory, 128);
+      strcat(directory, "/");
+      char *dest = strcat(directory, args[1]);
+      chdir(dest);
+    }
+  }else{
+    chdir(getenv("HOME"));
   }
-  
+}
+
+void printWD(){
+  char directory[129];
+  printf("%s\n", getcwd(directory, 128));
+}
+
+void exitProgram(){
+  exit(1);
+}
+
+//add forking here.
+int callCommand(char **args){
+  int pid = fork(), status;
+  if((pid == 0) && execvp(args[0], args) == -1){
+    fprintf(stderr, error_message);
+    kill(getpid(), SIGTERM);
+  }
   else if(doWait == 1) {
     wait(&status);
   }
-
-
-  free(splitInput);
-  free(args);
-  return 1; 
 }
 
 int main(int argc, char *argv[]){
@@ -55,38 +67,34 @@ int main(int argc, char *argv[]){
   char userInput[129];
   while(!quit){
     printf("mysh (%d)> ", commandCount++);
+    fflush(stdout);
     if(fgets(userInput, 129, stdin)==NULL){
       fprintf(stderr, error_message);
     }
     char *newlinePos = strchr(userInput, '\n');
     *newlinePos = '\0';
     char *andChar = strchr(userInput, '&');
-    
-
     if(andChar != NULL) {
       doWait = 0;
-      *andChar = '\0'; 
-    } 
+      *andChar = '\0';
+    }
     else {
       doWait = 1;
     }
-
-    if(strcmp("cd", userInput) == 0){
-      changeDir(userInput);
+    parseInput(userInput);
+    if(strcmp("cd", parsedArgs[0]) == 0){
+      changeDir(parsedArgs);
     }
-    else if(strcmp("pwd", userInput) == 0){
+    else if(strcmp("pwd", parsedArgs[0]) == 0){
       printWD();
     }
-    else if(strcmp("exit", userInput) == 0){
+    else if(strcmp("exit", parsedArgs[0]) == 0){
       exitProgram();
       quit = 1;
     }
     else if(userInput != NULL){
-      if(!callCommand(userInput)){
-        //debug extra newline here
-        fprintf(stderr, error_message);
-      }
-      //everything BUT exit, cd, and pwd goes here
+      callCommand(parsedArgs);
     }
+    free(parsedArgs);
   }
 }
